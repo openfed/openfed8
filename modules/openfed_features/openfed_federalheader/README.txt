c73602edfd3f6a6e40e537517b310e57b13cbcc6
stefan.r <stefan.r@551886.no-reply.drupal.org>
2017-03-13 17:48:11 +0100

    Add Federal Header module

---
+++ modules/openfed_features/openfed_federalheader/README.txt
@@ -0,0 +1,5 @@
+FEDERAL HEADER MODULE
+=====================
+
+This module provides a standardized "Belgian federal header" block linking to
+www.belgium.be, as part of the Openfed distribution.
---
+++ modules/openfed_features/openfed_federalheader/src/Plugin/Block/FederalHeaderBlock.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\openfed_federalheader\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a Federal header block.
+ *
+ * @Block(
+ *   id = "federal_header_block",
+ *   admin_label = @Translation("Federal header"),
+ * )
+ */
+class FederalHeaderBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $langcode = \Drupal::languageManager()->getCurrentLanguage()->getId();
+    return [
+      '#theme' => 'federalheader_block',
+      '#module_path' => base_path() . drupal_get_path('module', 'openfed_federalheader'),
+      '#other_information' => t('Other information and services: <span><a href=":link">www.belgium.be</a></span>', [':link' => 'http://www.belgium.be/' . $langcode]),
+      '#attached' => [
+        'library' => [
+          'openfed_federalheader/header',
+        ],
+      ],
+    ];
+  }
+
+}
