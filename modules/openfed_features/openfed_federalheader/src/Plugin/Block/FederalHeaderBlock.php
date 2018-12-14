8fc9a806d86d2b15b0ea6c15cd370fcbc095f4b2
rutiolma <rutiolma@gmail.com>
2018-12-14 16:15:27 +0100

    Issue #58: updated federal header to use a https link

--- modules/openfed_features/openfed_federalheader/src/Plugin/Block/FederalHeaderBlock.php
+++ modules/openfed_features/openfed_federalheader/src/Plugin/Block/FederalHeaderBlock.php
@@ -22,7 +22,7 @@ class FederalHeaderBlock extends BlockBase {
     return [
       '#theme' => 'federalheader_block',
       '#module_path' => base_path() . drupal_get_path('module', 'openfed_federalheader'),
-      '#other_information' => t('Other information and services: <span><a href=":link">www.belgium.be</a></span>', [':link' => 'http://www.belgium.be/' . $langcode]),
+      '#other_information' => t('Other information and services: <span><a href=":link">www.belgium.be</a></span>', [':link' => 'https://www.belgium.be/' . $langcode]),
       '#attached' => [
         'library' => [
           'openfed_federalheader/header',
